771d6fe28809fd0dcdb38dacd98d345bf9915baa
Fernando Camargo <camargodelbuono@gmail.com>
2022-01-06 23:47:16 +0100

    💾

--- src/components/widgets/video/helpers.js
+++ src/components/widgets/video/helpers.js
@@ -1,25 +1,31 @@
 import { Buffer } from 'buffer';
 import crypto from 'crypto';
 
-export function sign({
-  apiKey = 'TrZfzjeUSRKfT70PqXmzJg',
-  apiSecret = 'ZT2qZE8ggMaFPkZBRXfBkOuApfpShqfub1RC',
-  meetingNumber = 4373219238,
-  password = 'VnZkNmlpcjN2dE1KOHBUYnowaisxZz09',
-  role = 1,
-  userName = 'deus morto',
-} = {}) {
+export const sign = ({
+  apiKey,
+  apiSecret,
+  meetingNumber,
+  password,
+  role,
+  userName,
+}) => {
   const timestamp = new Date().getTime() - 30000;
-  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
-    'base64'
-  );
-  const hash = crypto
-    .createHmac('sha256', apiSecret)
-    .update(msg)
-    .digest('base64');
   const signature = Buffer.from(
-    `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
+    [
+      apiKey,
+      meetingNumber,
+      timestamp,
+      role,
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(
+          Buffer.from(
+            [apiKey, meetingNumber, timestamp, role].join('')
+          ).toString('base64')
+        )
+        .digest('base64'),
+    ].join('.')
   ).toString('base64');
 
   return { apiKey, meetingNumber, password, signature, userName };
-}
+};
--- src/components/widgets/video/hooks.js
+++ src/components/widgets/video/hooks.js
@@ -5,7 +5,7 @@ import { useZoom } from 'hooks/clients';
 
 import { sign } from './helpers';
 
-export default (props) => {
+export default () => {
   const ref = useRef();
   const client = useZoom();
   const { pending: fetching, watch } = useLatency();
@@ -18,7 +18,18 @@ export default (props) => {
           language: 'en-US',
           zoomAppRoot: ref.current,
         })
-        .then(() => client.join(sign()))
+        .then(() =>
+          client.join(
+            sign({
+              apiKey: 'TrZfzjeUSRKfT70PqXmzJg',
+              apiSecret: 'ZT2qZE8ggMaFPkZBRXfBkOuApfpShqfub1RC',
+              meetingNumber: 4373219238,
+              password: 'VnZkNmlpcjN2dE1KOHBUYnowaisxZz09',
+              role: 1,
+              userName: 'deus morto',
+            })
+          )
+        )
         .then(() => client.record('start'))
     );
 
@@ -35,176 +46,3 @@ export default (props) => {
 
   return { fetching, ref };
 };
-
-/*
-import { Buffer } from 'buffer';
-import crypto from 'crypto';
-import { useEffect, useRef } from 'react';
-
-export const getCredentials = () => ({
-  apiKey: 'TrZfzjeUSRKfT70PqXmzJg',
-  apiSecret: 'ZT2qZE8ggMaFPkZBRXfBkOuApfpShqfub1RC',
-  meetingNumber: 4373219238,
-  passWord: 'VnZkNmlpcjN2dE1KOHBUYnowaisxZz09',
-  role: 1,
-  userName: 'deus morto',
-});
-
-export function generateSignature({
-  apiKey,
-  apiSecret,
-  meetingNumber,
-  passWord,
-  role,
-  userName,
-}) {
-  const timestamp = new Date().getTime() - 30000;
-  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
-    'base64'
-  );
-  const hash = crypto
-    .createHmac('sha256', apiSecret)
-    .update(msg)
-    .digest('base64');
-  const signature = Buffer.from(
-    `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
-  ).toString('base64');
-
-  return { apiKey, meetingNumber, passWord, signature, userName };
-}
-
-export default () => {
-  const ref = useRef();
-
-  useEffect(() => {
-    window.navigator.mediaDevices
-      .getUserMedia({ audio: true, video: true })
-      .then(() =>
-        import('@zoomus/websdk').then(({ ZoomMtg }) => {
-          ZoomMtg.setZoomJSLib(`//${window.location.host}/zoom`, '/lib/av');
-          ZoomMtg.preLoadWasm();
-          ZoomMtg.prepareWebSDK();
-
-          return new Promise((success, error) =>
-            ZoomMtg.init({
-              disablePreview: true,
-              leaveUrl: window.location.href,
-              meetingInfo: ['host'],
-              error,
-              success,
-            })
-          )
-            .then(() => generateSignature(getCredentials()))
-            .then(
-              (signature) =>
-                new Promise((success, error) =>
-                  ZoomMtg.join({ ...signature, error, success })
-                )
-            );
-        })
-      );
-
-    return () => console.log('unmount();');
-  }, []);
-
-  return { ref };
-};
-*/
-
-/*
-import { Buffer } from 'buffer';
-import crypto from 'crypto';
-import noop from 'lodash/noop';
-import { ZoomMtg } from '@zoomus/websdk';
-import { useEffect, useRef } from 'react';
-
-export function generateSignature(
-  apiKey = 'TrZfzjeUSRKfT70PqXmzJg',
-  apiSecret = 'ZT2qZE8ggMaFPkZBRXfBkOuApfpShqfub1RC',
-  meetingNumber = 4373219238,
-  role = 1,
-  passWord = 'VnZkNmlpcjN2dE1KOHBUYnowaisxZz09',
-  userName = 'deus morto'
-) {
-  const timestamp = new Date().getTime() - 30000;
-  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
-    'base64'
-  );
-  const hash = crypto
-    .createHmac('sha256', apiSecret)
-    .update(msg)
-    .digest('base64');
-  const signature = Buffer.from(
-    `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
-  ).toString('base64');
-
-  return { apiKey, meetingNumber, passWord, signature, userName };
-}
-
-export class Client {
-  static destroy = () =>
-    new Promise((success, error) =>
-      ZoomMtg.getCurrentMeetingInfo({ error, success })
-    )
-      .then(Client.leave)
-      .catch(noop);
-
-  static init = () =>
-    new Promise((success, error) => {
-      ZoomMtg.setZoomJSLib(`//${window.location.host}/zoom`, '/lib/av');
-      ZoomMtg.preLoadWasm();
-      ZoomMtg.prepareWebSDK();
-
-      return ZoomMtg.init({
-        leaveUrl: window.location.href,
-        screenShare: true,
-        error,
-        success,
-      });
-    }).then(this.join);
-
-  static join = () =>
-    new Promise((success, error) =>
-      ZoomMtg.join({ ...generateSignature(), error, success })
-    );
-
-  static leave = () =>
-    new Promise((success, error) => ZoomMtg.leaveMeeting({ error, success }));
-}
-
-export default () => {
-  const ref = useRef();
-
-  useEffect(() => {
-    Client.init();
-
-    return () => Client.destroy();
-  }, []);
-
-  return { ref };
-};
-*/
-
-/*
-import Zoom from '@zoomus/websdk/embedded';
-import { useEffect, useRef } from 'react';
-
-
-
-export const client = Zoom.createClient();
-
-export default () => {
-  const ref = useRef();
-
-  useEffect(() => {
-    client
-      .init({ debug: false, language: 'en-US', zoomAppRoot: ref.current })
-      .then(() => client.join(generateSignature()))
-      .then(() => client.record('start'));
-
-    return () => client.leaveMeeting();
-  }, []);
-
-  return { ref };
-};
-*/
